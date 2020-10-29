16269ee6b4ac1b6d241d64094caac949d2cba785
mtadese <mtadese@gmail.com>
2020-10-29 21:27:52 +0100

    first commit

---
+++ src/components/Greet.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// function Greet() {
+// 	return <h1>Hello Mike</h1>
+// }
+
+const Greet = () => 
+<div>
+    <h1>Hi Micheal</h1>
+ 
+    This is your first React App. <br/>
+    ...using the Class Component. <br/>
+    ...keep coding. Cheers!
+
+</div>
+export default Greet
