a771adbc28d92485425251a33f930d0cc5a84e89
mtadese <mtadese@gmail.com>
2020-11-03 16:17:51 +0100

    changes for ver2

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -6,11 +6,16 @@ import React from 'react'
 
 const Greet = () => 
 <div>
+    
     <h1>Hi Micheal</h1>
  
     This is your first React App. <br/>
     ...using the Class Component. <br/>
     ...keep coding. Cheers!
+
+    <h1>Hello from V2</h1>
+    
+    This is an update to your first reactApp. Congrats!
 
 </div>
 export default Greet
